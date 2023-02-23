da13a655a02667d681f16adbcb762e09161c290c
Daniela Mical <daniela.m.torrez14@gmail.com>
2023-02-22 21:05:08 -0400

    Primeiro commit

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
         </div>
         ').appendTo(novoItem);
         $(novoItem).appendTo('ul');
-        $(novoItem).fadeIn();
+        $(novoItem).fadeIn(5000);
         $('endereco-img-nova').val('')
     })
 })
