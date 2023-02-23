a07094591fbf2d85aa6c4f4a3403f1330dba088d
Daniela Mical <daniela.m.torrez14@gmail.com>
2023-02-22 21:11:07 -0400

    Primeiro commit

--- main.js
+++ main.js
@@ -11,14 +11,14 @@ $(document).ready(function() {
         e.preventDefault();
         const enderecoDaNovaImagem = $('endereco-img-nova').val();
         const novoItem = $('<li style="display: none" ></li>');
-        $('<img src="$(enderecoDaNovaImagem)" />').appendTo(novoItem);
-        $('
+        $(<img src="$(enderecoDaNovaImagem)" /> ).appendTo(novoItem);
+        $(
         <div class="overlay-img-link">
         <a href="$(enderecoDaNovaImagem)" target="_blank" title="Ver imagem em tamanho real">
             Ver imagem em tamanho real
         </a>
         </div>
-        ').appendTo(novoItem);
+        ).appendTo(novoItem);
         $(novoItem).appendTo('ul');
         $(novoItem).fadeIn(1000);
         $('endereco-img-nova').val('')
